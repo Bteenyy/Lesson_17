004aba562f8399b148ae152fd22dc1a1c185b105
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2023-11-26 12:32:28 +0300

    first commit

---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:config/${launch}.properties"
+})
+public interface WebDriverConfig extends Config {
+
+    @Key("browser")
+    @DefaultValue("CHROME")
+    String browser();
+
+    @Key("size")
+    @DefaultValue("1920x1080")
+    String size();
+
+    @Key("version")
+    @DefaultValue("100.0")
+    String version();
+
+    @Key("remoteUrl")
+    String getRemoteUrl();
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,8 +2,10 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebDriverConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,15 +16,15 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
-
     @BeforeAll
     static void beforeAll() {
+        WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
         Configuration.baseUrl = "https://scalablesolutions.io/";
-        Configuration.browser = System.getProperty("browserName", "chrome");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.browser = System.getProperty("browserName", config.browser());
+        Configuration.browserSize = System.getProperty("browserSize", config.size());
+        Configuration.browserVersion = System.getProperty("browserVersion", config.version());
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = System.getProperty("selenoidAddress");
+        Configuration.remote = System.getProperty("selenoidAddress",config.getRemoteUrl());
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
